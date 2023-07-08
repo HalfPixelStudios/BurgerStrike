6a0cdcd916bb9b1edea57ab85dfb1eab9c31449b
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-07-08 16:38:30 -0400

    new design for burger display

--- scripts/ui/BurgerDisplay.gd
+++ scripts/ui/BurgerDisplay.gd
@@ -1,1 +1,11 @@
-extends VBoxContainer
+extends Control
+
+@onready var item_display: PackedScene = preload("res://scenes/ItemDisplay.tscn")
+
+const OFFSET = 10
+
+func add_item(item):
+	var inst = item_display.instantiate()
+	item_display.position = Vector2(0, -1 * OFFSET * get_children().size())
+	add_child(inst)
+	
